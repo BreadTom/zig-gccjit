718c10fa11a8c5afac7b85be41f57bf46b56b026
Kael Andrew Alonzo Franco <kaelfandrew@gmail.com>
2025-02-09 04:15:57 -0500

    Add GCC copyright, change gccjit enum, and add more Air To gccjit type

--- src/codegen/gccjit.zig
+++ src/codegen/gccjit.zig
@@ -29,12 +29,40 @@ const dev = @import("../dev.zig");
 
 const Error = error{ OutOfMemory, CodegenFail };
 
-pub fn getGCCJIT_Type_FromInst(inst: Air.Inst.Ref) c_int {
+pub fn getGCCJITenum_Types_FromInst_C_Only(inst: Air.Inst.Ref) ?gccjit.gcc_jit_types {
     switch (inst) {
-        .c_long_type => {
-            return gccjit.GCC_JIT_TYPE_LONG;
-        },
-
-        else => return 9,
+        .bool_type,
+        .u1_type,
+        => return gccjit.GCC_JIT_TYPE_BOOL,
+        .u8_type => return gccjit.GCC_JIT_TYPE_UINT8_T,
+        .i8_type => return gccjit.GCC_JIT_TYPE_INT8_T,
+        .u16_type => return gccjit.GCC_JIT_TYPE_UINT16_T,
+        .i16_type => return gccjit.GCC_JIT_TYPE_INT16_T,
+        .u32_type => return gccjit.GCC_JIT_TYPE_UINT32_T,
+        .i32_type => return gccjit.GCC_JIT_TYPE_INT32_T,
+        .u64_type => return gccjit.GCC_JIT_TYPE_UINT64_T,
+        .i64_type => return gccjit.GCC_JIT_TYPE_INT64_T,
+        .u128_type => return gccjit.GCC_JIT_TYPE_UINT128_T,
+        .i128_type => return gccjit.GCC_JIT_TYPE_INT128_T,
+        .c_char_type => return gccjit.GCC_JIT_TYPE_CHAR,
+        .c_short_type => return gccjit.GCC_JIT_TYPE_SHORT,
+        .c_ushort_type => return gccjit.GCC_JIT_TYPE_UNSIGNED_SHORT,
+        .c_int_type => return gccjit.GCC_JIT_TYPE_INT,
+        .c_uint_type => return gccjit.GCC_JIT_TYPE_UNSIGNED_INT,
+        .c_long_type => return gccjit.GCC_JIT_TYPE_LONG,
+        .c_ulong_type => return gccjit.GCC_JIT_TYPE_UNSIGNED_LONG,
+        .c_longlong_type => return gccjit.GCC_JIT_TYPE_LONG_LONG,
+        .c_ulonglong_type => return gccjit.GCC_JIT_TYPE_UNSIGNED_LONG_LONG,
+        .f32_type => return gccjit.GCC_JIT_TYPE_FLOAT,
+        .f64_type => return gccjit.GCC_JIT_TYPE_DOUBLE,
+        else => return null,
     }
+}
+
+pub fn getGCCJITstruct_Type_FromInst_C_Only(ctxt: *gccjit.gcc_jit_context, inst: Air.Inst.Ref) ?*gccjit.gcc_jit_type {
+    const enum_type_from_air: ?gccjit.gcc_jit_types = getGCCJITenum_Types_FromInst_C_Only(inst);
+    if (enum_type_from_air == null) {
+        return null;
+    }
+    return gccjit.gcc_jit_context_get_type(ctxt, @as(gccjit.gcc_jit_types, enum_type_from_air));
 }
